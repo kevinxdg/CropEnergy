995ea0dad03f96d395f16ce2e208224bc4e00a22
kevinxdg <kevinxdg@gmail.com>
2023-02-16 14:15:25 +0800

    Default Changelist

--- CODE/Pic_Energy.py
+++ CODE/Pic_Energy.py
@@ -1,3 +1,19 @@
 #coding=utf-8
-import matplotlib.pyplot as mp
+import matplotlib.pyplot as plt
+from cmodel import *
 
+data_file = r'Result_Energy_2023_02_16_10_52_53.xlsx'
+data_path = result_dir + data_file
+
+print(data_path)
+
+excel_tool.load_workbook(data_path)
+data = excel_tool.cell_DataFrame(index=0)
+print(data)
+
+pic_data = data.colDataFrame(1).T
+
+print(pic_data.array)
+plt.bar(data.index.tolist(),pic_data.list)
+print(data.index.tolist())
+plt.show()
--- CODE/energyCal.py
+++ CODE/energyCal.py
@@ -1,15 +1,30 @@
 from cmodel import *
 import numpy as np
-data_path = 'H:\\Python\\Projects\\CropEnergy\\Data\\'
+
+data_file = r'AgriInputs2208_For_energy.xlsx'
+data_path = data_dir + data_file
+
+para_file = r'Parameters.xlsx'
+para_path = data_dir + para_file
+
+save_file = r'Result_Energy_' + time_tool.formatted_string() + '.xlsx'
+save_path = result_dir + save_file
+
 exl = ExcelHelper()
-exl.load_workbook(data_path + 'AgriInputs2208.xlsx')
+exl.load_workbook(data_path )
 data = exl.cell_DataFrame()
-exl.load_workbook(data_path + 'Parameters.xlsx')
-tmp_para = exl.cell_DataFrame()
+
+exl.load_workbook(para_path)
+tmp_para = exl.cell_DataFrame(index=0)
+print(tmp_para)
+
 var_index = list(range(20))
 var_index = var_index[:4] + var_index[6:]
 para = tmp_para.T.colsDataFrame(var_index)
 para_value = para.rowDataFrame(1)
+print('------')
+print(para_value)
+
 (rd,cd)=data.shape
 (rp,cp)=para_value.shape
 para_value.resize(rd,cp,amend_value=False,columns=para_value.columns, index=data.index)
@@ -20,13 +35,13 @@ para_value.iloc[(2000-1949):,3] = para_N_2019
 
 data = DataFrameClass(data.fillna(0))
 result = data * para_value
+result.columns = para_value.columns
 print(result)
 
-tm = timeTool()
-fname = data_path + 'Result\\Result_Energy_' + tm.formatted_string() + '.xlsx'
+
 exl = ExcelHelper()
 exl.copy_DataFrame(result,withtitles=True, withindex=True, indextitle='Year')
-exl.save_workbook(fname)
+exl.save_workbook(save_path)
 
 direct_energy = result.colsDataFrame([0,1,7]).sum(axis=1)
 indirect_energy = result.colsDataFrame(list(range(2,7))+list(range(8,18))).sum(axis=1)
